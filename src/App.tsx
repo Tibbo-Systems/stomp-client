aca98ca78ac5f9cc24fb6d979c81464e39ebe6cf
andrey.plis <andrey.plis@tibbo.com>
2020-04-08 14:37:49 +0300

    added subscriber

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Client, Message} from '@stomp/stompjs';
+import {Client, Message, StompSubscription} from '@stomp/stompjs';
 import './App.css';
 
 interface IProps {
@@ -12,6 +12,8 @@ interface IState {
 
 class App extends React.Component<IProps, IState> {
 
+    private client!: Client;
+    private sub!: StompSubscription;
 
     constructor(props: any) {
         super(props);
@@ -35,7 +37,7 @@ class App extends React.Component<IProps, IState> {
             body: JSON.stringify(data)
         });
         const res = await response.json();
-        const client = new Client({
+        this.client = new Client({
             brokerURL: "ws://localhost:8080/rest/v1/stomp",
             connectHeaders: {
                 Authorization: "Bearer " + res.token,
@@ -47,12 +49,12 @@ class App extends React.Component<IProps, IState> {
             heartbeatIncoming: 4000,
             heartbeatOutgoing: 4000
         });
-        client.onConnect = function (frame) {
+        this.client.onConnect = function (frame) {
             // Do something, all subscribes must be done is this callback
             // This is needed because this will be executed after a (re)connect
         };
 
-        client.onStompError = function (frame) {
+        this.client.onStompError = function (frame) {
             // Will be invoked in case of error encountered at Broker
             // Bad login/passcode typically will cause an error
             // Complaint brokers will set `message` header with a brief message. Body may contain details.
@@ -60,17 +62,38 @@ class App extends React.Component<IProps, IState> {
             console.log('Broker reported error: ' + frame.headers['message']);
             console.log('Additional details: ' + frame.body);
         };
-        client.activate();
+        this.client.activate();
     }
 
+    subscribe = (): void => {
+        this.sub = this.client.subscribe("/user/v1/contexts/users.admin.devices.virtual/events/event1", (message) => {
+            console.log(message.body)
+        });
+        //"/user/v1/contexts/users.admin.devices.virtual/events/" + event
+    }
+
+    unsubscribe = (): void => {
+        this.sub.unsubscribe();
+        //"/user/v1/contexts/users.admin.devices.virtual/events/" + event
+    }
+
+    disconnect = (): void => {
+        this.client.deactivate();
+    }
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
             <div className="App">
                 <header className="App-header">
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
+                    <button onClick={this.subscribe}>
+                        Subscribe
+                    </button>
+                    <button onClick={this.unsubscribe}>
+                        Unsubscribe
+                    </button>
+                    <button onClick={this.disconnect}>
+                        Disconnect
+                    </button>
                     <a
                         className="App-link"
                         href="https://reactjs.org"
